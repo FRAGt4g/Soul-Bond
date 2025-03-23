96c9793ef62116d1315aeff32b7810c5744278e4
Miles F <miles.fritzmather@gmail.com>
2025-03-23 10:18:31 -0500

    testing commit in normal project window

--- Soul-Bound/Assets/Scripts/Test File.cs
+++ Soul-Bound/Assets/Scripts/Test File.cs
@@ -5,12 +5,18 @@ public class TestFile : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+        Debug.Log(z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+        Debug.Log(z);
     }
 }
